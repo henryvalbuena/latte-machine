5a34de1b3533c001d4244b78a5275cb3b3c8ecc2
Henry Valbuena <dev.henryvm@gmail.com>
2020-06-22 23:57:00 -0400

    Updates

---
+++ precache-manifest.ab28c8d93dc2b5c2cd2cdcba99f48f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6daed91fada2ef03f12ee8411f6ba6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d19e99a5e992a0a34b4",
+    "url": "/static/css/main.1724c5a0.chunk.css"
+  },
+  {
+    "revision": "c6d95d0071a402f3cf46",
+    "url": "/static/js/2.b9244223.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9244223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d19e99a5e992a0a34b4",
+    "url": "/static/js/main.7a282dfe.chunk.js"
+  },
+  {
+    "revision": "d624e2eb342c094962af",
+    "url": "/static/js/runtime-main.1369d75c.js"
+  }
+]);
